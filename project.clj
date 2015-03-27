7105a1d7c82c3f687a7d66d9d36c6c6002e8f361
Ben Mather <bwhmather@bwhmather.com>
2015-03-27 17:00:39 +0000

    add lein-bikeshed and lein-kibit as dev dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/bwhmather/clj-linemode"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[lein-bikeshed "0.2.0"]
+                             [lein-kibit "0.0.8"]]}})
