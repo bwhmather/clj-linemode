6325b1e4d6a9d0899d3b4e8d5bc695fcef95e825
Ben Mather <bwhmather@bwhmather.com>
2015-03-27 16:32:19 +0000

    Initial commit of leiningen generated project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to linemode
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/linemode/core_test.clj
@@ -0,0 +1,7 @@
+(ns linemode.core-test
+  (:require [clojure.test :refer :all]
+            [linemode.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
