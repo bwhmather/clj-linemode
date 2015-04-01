99d9b47eee4f89893fe2710edfee8fa07e345598
James Henderson <james@jarohen.me.uk>
2015-04-01 11:45:50 +0100

    refactoring

--- test/linemode/core_test.clj
+++ test/linemode/core_test.clj
@@ -2,13 +2,14 @@
   (:import [java.io ByteArrayOutputStream])
   (:require [clojure.test :refer :all]
             [linemode.core :refer [run-commands]]
-            [linemode.drivers.star :refer [star-printer]]))
+            [linemode.drivers.star :refer [with-star-printer]]))
 
 
 (deftest compile-test
   (testing "Simple writes should be copied straight to output"
-    (let [output (new ByteArrayOutputStream)
-          printer (star-printer output)]
-      ; TODO
-      (run-commands printer [[:write "Hello World"]])
+    (let [output (new ByteArrayOutputStream)]
+      (with-star-printer output
+        (fn [printer]
+          (run-commands printer [[:write "Hello World"]])))
+
       (is (= (.toString output) "Hello World")))))
